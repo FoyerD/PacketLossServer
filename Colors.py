714937947338189dde26e861c3d211744122ba0e
FoyerD <amitfoye@post.bgu.ac.il>
2025-01-14 17:33:38 +0200

    added full colors file

--- Colors.py
+++ Colors.py
@@ -1,12 +1,90 @@
-HEADER = '\033[95m'
-OKBLUE = '\033[94m'
-OKCYAN = '\033[96m'
-OKGREEN = '\033[92m'
-WARNING = '\033[93m'
-FAIL = '\033[91m'
-ENDC = '\033[0m'
-BOLD = '\033[1m'
-UNDERLINE = '\033[4m'
+BLACK = "\033[0;30m"
+RED = "\033[0;31m"
+GREEN = "\033[0;32m"
+BROWN = "\033[0;33m"
+BLUE = "\033[0;34m"
+PURPLE = "\033[0;35m"
+CYAN = "\033[0;36m"
+LIGHT_GRAY = "\033[0;37m"
+DARK_GRAY = "\033[1;30m"
+LIGHT_RED = "\033[1;31m"
+LIGHT_GREEN = "\033[1;32m"
+YELLOW = "\033[1;33m"
+LIGHT_BLUE = "\033[1;34m"
+LIGHT_PURPLE = "\033[1;35m"
+LIGHT_CYAN = "\033[1;36m"
+LIGHT_WHITE = "\033[1;37m"
+BOLD = "\033[1m"
+FAINT = "\033[2m"
+ITALIC = "\033[3m"
+UNDERLINE = "\033[4m"
+BLINK = "\033[5m"
+NEGATIVE = "\033[7m"
+CROSSED = "\033[9m"
+END = "\033[0m"
 
-def blue_str(str):
-    return OKBLUE + str + ENDC
+def blue_str(s):
+    return BLUE + s + END
+
+def red_str(s):
+    return RED + s + END
+
+def green_str(s):
+    return GREEN + s + END
+
+def brown_str(s):
+    return BROWN + s + END
+
+def purple_str(s):
+    return PURPLE + s + END
+
+def cyan_str(s):
+    return CYAN + s + END
+
+def light_gray_str(s):
+    return LIGHT_GRAY + s + END
+
+def dark_gray_str(s):
+    return DARK_GRAY + s + END
+
+def light_red_str(s):
+    return LIGHT_RED + s + END
+
+def light_green_str(s):
+    return LIGHT_GREEN + s + END
+
+def yellow_str(s):
+    return YELLOW + s + END
+
+def light_blue_str(s):
+    return LIGHT_BLUE + s + END
+
+def light_purple_str(s):
+    return LIGHT_PURPLE + s + END
+
+def light_cyan_str(s):
+    return LIGHT_CYAN + s + END
+
+def light_white_str(s):
+    return LIGHT_WHITE + s + END
+
+def bold_str(s):
+    return BOLD + s + END
+
+def faint_str(s):
+    return FAINT + s + END
+
+def italic_str(s):
+    return ITALIC + s + END
+
+def underline_str(s):
+    return UNDERLINE + s + END
+
+def blink_str(s):
+    return BLINK + s + END
+
+def negative_str(s):
+    return NEGATIVE + s + END
+
+def crossed_str(s):
+    return CROSSED + s + END
